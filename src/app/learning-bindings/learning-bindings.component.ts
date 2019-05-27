9a041762cd7d91fc6ef13aa9f9bf256f9a3def00
Allan Grevas <stratosgrevas@gmail.com>
2019-05-27 10:38:39 -0300

    binding from logic to view

--- src/app/learning-bindings/learning-bindings.component.ts
+++ src/app/learning-bindings/learning-bindings.component.ts
@@ -8,25 +8,41 @@ import { Component, OnInit } from '@angular/core';
 
 export class LearningBindingsComponent implements OnInit {
 
+	public mouseClickCount: number;
+	public mouseOverCount: number;
+	public userName: string;
+	public userEmail: string;
+
+	public constructor(){
+		this.mouseClickCount = 0;
+		this.mouseOverCount = 0;
+		this.userName = "";
+		this.userEmail = "";
+	}
+
 	// mouse events
 	public onClick(){
 		console.log('Evento onClick disparado!');
+		this.mouseClickCount++;
 	}
 
 	public onMouseOver(){
 		console.log('Evento onMouseOver disparado!');
+		this.mouseOverCount++;
 	}
 
 	// key events
-	public onKeyDown(){
+	public onKeyDown(event: any){
 		console.log('Evento onKeyDown disparado!');
+		console.log(event);
+		this.userName = event.target.value;
 	}
 
-	public onKeyUp(){
+	public onKeyUp(event: any){
 		console.log('Evento onKeyUp disparado!');
+		console.log(event);
+		this.userEmail = event.target.value;
 	}
-
-  constructor() { }
 
   ngOnInit() {
   }
