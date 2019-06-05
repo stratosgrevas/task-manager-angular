2330f529a4de9e5e9c99230aa0d5e8753d1db3a2
Allan Grevas <stratosgrevas@gmail.com>
2019-06-05 16:28:45 -0300

    removing TASKS constant from TaskService

--- src/app/tasks/shared/task.service.ts
+++ src/app/tasks/shared/task.service.ts
@@ -9,19 +9,6 @@ import "rxjs/add/operator/map";
 /* components imports */
 import { Task } from './task.model';
 
-const TASKS: Array<Task> = [
-	{ id: 1, title: 'Fazer tarefa 1' },
-	{ id: 2, title: 'Fazer tarefa 2' },
-	{ id: 3, title: 'Fazer tarefa 3' },
-	{ id: 4, title: 'Fazer tarefa 4' },
-	{ id: 5, title: 'Fazer tarefa 5' },
-	{ id: 6, title: 'Fazer tarefa 6' },
-	{ id: 7, title: 'Fazer tarefa 7' },
-	{ id: 8, title: 'Fazer tarefa 8' },
-	{ id: 9, title: 'Fazer tarefa 9' },
-	{ id: 10, title: 'Fazer tarefa 10' }
-];
-
 @Injectable()
 
 export class TaskService{
