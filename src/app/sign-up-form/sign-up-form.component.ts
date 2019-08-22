3ee1037fdbbb4e989111118ba09a02aa4177a093
Allan Grevas <stratosgrevas@gmail.com>
2019-08-22 17:25:55 -0300

    adding SignUpFormComponent base template and adjustments

--- src/app/sign-up-form/sign-up-form.component.ts
+++ src/app/sign-up-form/sign-up-form.component.ts
@@ -3,7 +3,7 @@ import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms"
 
 @Component({
     selector: 'sign-up-form',
-    templateUrl: './sign-up-form-component.html'
+    templateUrl: './sign-up-form.component.html'
 })
 
 export class SignUpFormComponent {
@@ -17,7 +17,7 @@ export class SignUpFormComponent {
             passwordConfirmation: [null, [Validators.required]]
         })
     }
-    
+
     public signUpUser(){
         console.log("Formulário de sign up enviado !");
         console.log(this.userForm.value);
