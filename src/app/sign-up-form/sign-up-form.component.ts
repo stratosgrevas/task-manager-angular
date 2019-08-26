ccfd107bbb6e02f79c82c55b22386947ce0ea2d6
Allan Grevas <stratosgrevas@gmail.com>
2019-08-26 17:52:06 -0300

    adding custom validator to SignUpComponent for password matching

--- src/app/sign-up-form/sign-up-form.component.ts
+++ src/app/sign-up-form/sign-up-form.component.ts
@@ -19,7 +19,7 @@ export class SignUpFormComponent {
             email: [null, [Validators.required, Validators.email]],
             password: [null, [Validators.required, Validators.minLength(8)]],
             passwordConfirmation: [null, [Validators.required]]
-        })
+        }, { validator: this.passwordConfirmationValidator })
 
         this.formUtils = new FormUtils(this.form);
     }
@@ -27,5 +27,14 @@ export class SignUpFormComponent {
     public signUpUser(){
         console.log("Formulário de sign up enviado !");
         console.log(this.form.value);
+    }
+
+    public passwordConfirmationValidator(form: FormGroup){
+        if( form.get('password').dirty && form.get('password').value === form.get('passwordConfirmation').value ){
+            form.get('passwordConfirmation').setErrors(null);
+        }
+        else{
+            form.get('passwordConfirmation').setErrors({'mismatch':true});
+        }
     }
 }
